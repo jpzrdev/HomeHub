aa09b2fa6b898b85d644ca588ecd13c6b3906582
João Pedro Zieppe da Rocha <53254239+jpzrdev@users.noreply.github.com>
2025-12-15 22:35:46 -0300

    feat: add migrations to remove notify on below minimum quantity

--- homehub-backend/src/HomeHub.Infrastructure/Migrations/20251215012217_RemoveNotifyOnBelowMinimumQuantity.Designer.cs
+++ homehub-backend/src/HomeHub.Infrastructure/Migrations/20251215012217_RemoveNotifyOnBelowMinimumQuantity.Designer.cs
@@ -4,6 +4,7 @@ using HomeHub.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeHub.Infrastructure.Migrations
 {
     [DbContext(typeof(HomeHubContext))]
-    partial class HomeHubContextModelSnapshot : ModelSnapshot
+    [Migration("20251215012217_RemoveNotifyOnBelowMinimumQuantity")]
+    partial class RemoveNotifyOnBelowMinimumQuantity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ homehub-backend/src/HomeHub.Infrastructure/Migrations/20251215012217_RemoveNotifyOnBelowMinimumQuantity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveNotifyOnBelowMinimumQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotifyOnBelowMinimumQuantity",
+                table: "InventoryItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NotifyOnBelowMinimumQuantity",
+                table: "InventoryItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
